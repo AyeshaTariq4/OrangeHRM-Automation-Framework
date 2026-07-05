e9aa8d1a3f515bf59882a2b8753d2a0a51546828
AyeshaTariq4 <aishatariq104@gmail.com>
2026-07-06 04:54:54 +0500

    Added GitHub Actions Workflow

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,6 +10,10 @@ from pages.logout_page import LogoutPage
 
 # Chrome Options
 options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1920,1080")
 
 # Browser Launch
 service = Service(ChromeDriverManager().install())
