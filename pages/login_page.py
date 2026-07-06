d81b18c8a3242ec16cd8d8c13578b1b77241daeb
AyeshaTariq4 <aishatariq104@gmail.com>
2026-07-07 01:16:19 +0500

    Added explicit wait for login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,11 +1,23 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
 
     def login(self, username, password):
-        self.driver.find_element(By.NAME, "username").send_keys(username)
-        self.driver.find_element(By.NAME, "password").send_keys(password)
-        self.driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+        self.wait.until(
+            EC.visibility_of_element_located((By.NAME, "username"))
+        ).send_keys(username)
+
+        self.wait.until(
+            EC.visibility_of_element_located((By.NAME, "password"))
+        ).send_keys(password)
+
+        self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']"))
+        ).click()
